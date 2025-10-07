e61d921cbf453cd61883e920410f5f45c3f547f1
ManojGowda89 <mail@manojgowda.in>
2025-10-07 12:43:00 +0530

    disjf

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,50 +1,84 @@
-import { app as t, BrowserWindow as a } from "electron";
-import { createRequire as w } from "node:module";
-import { fileURLToPath as m } from "node:url";
-import n from "node:path";
-w(import.meta.url);
-const d = n.dirname(m(import.meta.url));
-process.env.APP_ROOT = n.join(d, "..");
-const l = process.env.VITE_DEV_SERVER_URL, _ = n.join(process.env.APP_ROOT, "dist-electron"), c = n.join(process.env.APP_ROOT, "dist");
-process.env.VITE_PUBLIC = l ? n.join(process.env.APP_ROOT, "public") : c;
-let e = null;
-function p() {
-  e = new a({
+import { app, BrowserWindow } from "electron";
+import { createRequire } from "node:module";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+createRequire(import.meta.url);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+process.env.APP_ROOT = path.join(__dirname, "..");
+const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
+const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
+const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
+let win = null;
+function createWindow() {
+  win = new BrowserWindow({
     width: 900,
     height: 700,
-    autoHideMenuBar: !0,
+    autoHideMenuBar: true,
     title: "PaySlipGenerator",
     // ✅ window title
-    icon: n.join(process.env.VITE_PUBLIC, "LOGO.png"),
+    icon: path.join(process.env.VITE_PUBLIC, "LOGO.png"),
     // ✅ app icon
     webPreferences: {
-      preload: n.join(d, "preload.mjs"),
-      contextIsolation: !0,
-      nodeIntegration: !1
+      preload: path.join(__dirname, "preload.mjs"),
+      contextIsolation: true,
+      nodeIntegration: false
     }
-  }), e.setMenu(null), e.webContents.on("did-finish-load", () => {
-    e == null || e.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  }), l ? e.loadURL(l) : e.loadFile(n.join(c, "index.html")), l || (e.webContents.on("context-menu", (s) => s.preventDefault()), e.webContents.on("before-input-event", (s, o) => {
-    o.control && o.shift && o.key.toLowerCase() === "i" && s.preventDefault();
-  })), e.webContents.session.on("will-download", (s, o) => {
-    const r = n.join(t.getPath("downloads"), o.getFilename());
-    o.setSavePath(r), o.on("updated", (f, i) => {
-      i === "progressing" && (o.isPaused() ? console.log("Download is paused") : console.log(`Received bytes: ${o.getReceivedBytes()}`));
-    }), o.once("done", (f, i) => {
-      console.log(i === "completed" ? `Download completed: ${r}` : `Download failed: ${i}`);
+  });
+  win.setMenu(null);
+  win.webContents.on("did-finish-load", () => {
+    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  });
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(RENDERER_DIST, "index.html"));
+  }
+  if (!VITE_DEV_SERVER_URL) {
+    win.webContents.on("context-menu", (e) => e.preventDefault());
+    win.webContents.on("before-input-event", (event, input) => {
+      if (input.control && input.shift && input.key.toLowerCase() === "i") {
+        event.preventDefault();
+      }
+    });
+  }
+  const ses = win.webContents.session;
+  ses.on("will-download", (event, item) => {
+    const savePath = path.join(app.getPath("downloads"), item.getFilename());
+    item.setSavePath(savePath);
+    item.on("updated", (event2, state) => {
+      if (state === "progressing") {
+        if (item.isPaused()) {
+          console.log("Download is paused");
+        } else {
+          console.log(`Received bytes: ${item.getReceivedBytes()}`);
+        }
+      }
+    });
+    item.once("done", (event2, state) => {
+      if (state === "completed") {
+        console.log(`Download completed: ${savePath}`);
+      } else {
+        console.log(`Download failed: ${state}`);
+      }
     });
   });
 }
-t.setName("PaySlipGenerator");
-t.on("window-all-closed", () => {
-  process.platform !== "darwin" && (t.quit(), e = null);
+app.setName("PaySlipGenerator");
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    win = null;
+  }
 });
-t.on("activate", () => {
-  a.getAllWindows().length === 0 && p();
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
-t.whenReady().then(p);
+app.whenReady().then(createWindow);
 export {
-  _ as MAIN_DIST,
-  c as RENDERER_DIST,
-  l as VITE_DEV_SERVER_URL
+  MAIN_DIST,
+  RENDERER_DIST,
+  VITE_DEV_SERVER_URL
 };
--- dist-electron/preload.mjs
+++ dist-electron/preload.mjs
@@ -1,1 +1,18 @@
-"use strict";const n=require("electron");n.contextBridge.exposeInMainWorld("ipcRenderer",{on(e,r){return n.ipcRenderer.on(e,(i,...o)=>r(i,...o))},off(e,r){return n.ipcRenderer.off(e,r)},send(e,...r){return n.ipcRenderer.send(e,...r)},invoke(e,...r){return n.ipcRenderer.invoke(e,...r)}});
+"use strict";
+const electron = require("electron");
+electron.contextBridge.exposeInMainWorld("ipcRenderer", {
+  on(channel, listener) {
+    return electron.ipcRenderer.on(channel, (event, ...args) => listener(event, ...args));
+  },
+  off(channel, listener) {
+    return electron.ipcRenderer.off(channel, listener);
+  },
+  send(channel, ...args) {
+    return electron.ipcRenderer.send(channel, ...args);
+  },
+  invoke(channel, ...args) {
+    return electron.ipcRenderer.invoke(channel, ...args);
+  }
+  // You can expose other APIs you need here.
+  // ...
+});
